95361733ff9a477298c8a257d8f37286f27467e7
IvanMuzik <ivanmuz86@gmail.com>
2023-02-19 20:55:40 +0300

    add comment in view controller

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // добавил элементы в сториборд
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
